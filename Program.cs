5fd0f195be40153b38ed0adef852735f20824a2f
SamikshaThakare <thakare.samiksha2006@gmail.com>
2022-01-28 19:15:19 +0530

    Program code has been published on git repo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+ namespace firstApp
+ {
+     class Program
+     {
+         public static void main (String [] args)
+         {
+             String FullName="Samiksha Sanjay Thakare";
+             Console.WriteLine("FullName:"+FullName);
+             Console.WriteLine("Hello------"+FullName);
+             Console.ReadLine();
+
+         }
+     }
+ }
